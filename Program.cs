3c72172770a7cd668dfb7b07563646aa83b1cd5a
RafaArturo <draco032@gmail.com>
2019-12-10 13:00:09 -0400

    Fix conflicts

--- Program.cs
+++ Program.cs
@@ -7,15 +7,9 @@ namespace AddressBookCs
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Contact c1 = new Contact("1", "Rafa", "809-000-0001", "example1@mail.com");
-            Contact c2 = new Contact("2", "Jhon", "809-000-0002", "example2@mail.com");
-            Contact c3 = new Contact("3", "Rafa", "809-000-0003", "example3@mail.com");
-=======
             
             
             //Create new addressBook
->>>>>>> 96b2e31... Add database CRUD
             AddressBook ab = new AddressBook();
             
             cli cli1 = new cli();
@@ -31,14 +25,10 @@ namespace AddressBookCs
             cli1.addressBookUsed.listContacts();
             
 
-<<<<<<< HEAD
-            cli1.run();
-=======
             //Initializing CLI
             //cli1.run();
             
             
->>>>>>> 96b2e31... Add database CRUD
 
         }
     }
